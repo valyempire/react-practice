c482baf23e316375da353895baf4e7c0bac33372
unknown <valy_empire@yahoo.com>
2023-06-12 12:39:19 +0300

    Added css to Carousel components also added settings for slider from slick carousel library

--- react-slick-carousel/src/components/Carousel.tsx
+++ react-slick-carousel/src/components/Carousel.tsx
@@ -1,12 +1,35 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "./Carousel.css";
 
 export const Carousel: React.FC = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
   return (
     <div className="carousel">
       <h2>Carousel Model</h2>
-      <Slider>
+      <Slider {...settings}>
         <div className="box">
           <h3>1</h3>
         </div>
