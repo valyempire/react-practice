9a4c2f18db0d0e04cfbb1e241bd7a850ff66b257
unknown <valy_empire@yahoo.com>
2023-06-13 14:06:54 +0300

    Created a data folder containing details about my work also added images for data

---
+++ react-slick-carousel/src/data/data.ts
@@ -0,0 +1,35 @@
+import lights_img from "../assets/lights1.png";
+import hangman_img from "../assets/hangman2.png";
+import smartbrain_img from "../assets/smart-brain.jpg";
+import pokedex_img from "../assets/Pokedex-feature.png";
+
+export const data = [
+  {
+    id: 1,
+    name: "LightsOut",
+    image: lights_img,
+    github: "https://github.com/valyempire",
+    live: "https://github.com/valyempire",
+  },
+  {
+    id: 2,
+    name: "SmartBrain",
+    image: smartbrain_img,
+    github: "https://github.com/valyempire",
+    live: "https://github.com/valyempire",
+  },
+  {
+    id: 3,
+    name: "Hangman",
+    image: hangman_img,
+    github: "https://github.com/valyempire",
+    live: "https://github.com/valyempire",
+  },
+  {
+    id: 4,
+    name: "Pokedex",
+    image: pokedex_img,
+    github: "https://github.com/valyempire",
+    live: "https://github.com/valyempire",
+  },
+];
