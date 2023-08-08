e5c3495fd36ee79d99eb108324ebb249047035ae
unknown <valy_empire@yahoo.com>
2023-08-08 22:57:13 +0300

    Created a function for checking if a todo is completed and a function for deleting a todo.

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -12,6 +12,18 @@ export interface TodoInterface {
 export const App: React.FC = () => {
   const [todos, setTodos] = useState<TodoInterface[]>([]);
 
+  const handleCheckedTodo = (index: number) => {
+    const newTodo = [...todos];
+    newTodo[index].completed = !newTodo[index].completed;
+    setTodos(newTodo);
+  };
+
+  const handleDeleteTodo = (index: number) => {
+    const newTodo = [...todos];
+    newTodo.splice(index, 1);
+    setTodos(newTodo);
+  };
+
   useEffect(() => {
     const getTodos = async () => {
       try {
@@ -28,8 +40,18 @@ export const App: React.FC = () => {
   return (
     <div>
       <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
+        {todos.map((todo, index) => (
+          <div>
+            <div
+              style={{ textDecoration: todo.completed ? "line-through" : "" }}
+            >
+              <li key={todo.id}>{todo.title}</li>
+            </div>
+            <button onClick={() => handleCheckedTodo(index)}>
+              {todo.completed ? "Completed" : "Incompleted"}
+            </button>
+            <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+          </div>
         ))}
       </ul>
     </div>
