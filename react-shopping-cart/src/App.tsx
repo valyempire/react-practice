e4aace63b600258dc8a26443fb2e4dea59b581d9
unknown <valy_empire@yahoo.com>
2023-08-18 22:59:11 +0300

    Created the Navbar Component

---
+++ react-shopping-cart/src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+
+export const App = () => {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<h1>Shop</h1>} />
+          <Route path="/contact" element={<h1>Contact</h1>} />
+          <Route path="/cart" element={<h1>Cart</h1>} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
---
+++ react-shopping-cart/src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import { ShoppingCart } from "phosphor-react";
+import "./navbar.css";
+
+export const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="links">
+        <Link to="/"> Shop </Link>
+        <Link to="/contact"> Contact </Link>
+        <Link to="/cart">
+          <ShoppingCart size={32} />
+        </Link>
+      </div>
+    </div>
+  );
+};
